770ef9df5896c60d4c5b9d8d408ab4de5c4dec2a
pswitala <p.switala@pslab.eu>
2020-01-03 17:18:21 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "dns_servers" {
+output "pscloud_dns_servers" {
   value       = aws_route53_zone.pscloud-primary.*.name_servers
 }
 
-output "domain_name" {
+output "pscloud_domain_name" {
   value       = aws_route53_zone.pscloud-primary.name
+}
+
+output "pscloud_zone_id" {
+  value = aws_route53_zone.pscloud-primary.zone_id
 }
